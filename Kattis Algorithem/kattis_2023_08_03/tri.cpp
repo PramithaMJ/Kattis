f9addb3090eb26120963a045519882aa3df5139d
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-08-03 09:04:24 +0530

    ss

---
+++ Kattis Algorithem/kattis_2023_08_03/tri.cpp
@@ -0,0 +1,25 @@
+  
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <set>
+using namespace std;
+int main() {
+    string line;
+    getline(cin, line);
+    istringstream iss(line);
+    vector<string> word{istream_iterator<string>{iss}, istream_iterator<string>{}};
+    set<string> s;
+    for (auto& words : word) {
+        if (s.find(words) != s.end()) {
+            cout << "no";
+            return 0;
+        }
+        s.insert(words);
+    }
+    cout << "yes";
+    return 0;
+}
