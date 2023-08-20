30f4960f186bb0ff3cce61c5300175cd1a68a1dd
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-08-20 09:19:45 +0530

    updated test

--- codeTask.cpp
+++ codeTask.cpp
@@ -1,7 +1,26 @@
-
 #include <iostream>
+#include <string>
+
 using namespace std;
-int main() {
-    
+
+int main()
+{
+
+    // test 1anser
+    int num;
+    cin >> num;
+    while (num != -1)
+    {
+        int speed, time, distance = 0, preTime = 0;
+        for (int i = 0; i < num; i++)
+        {
+            cin >> speed >> time;
+            distance += speed * (time - preTime);
+            preTime = time;
+        }
+        cout << distance << " miles" << endl;
+        cin >> num;
+    }
+ 
     return 0;
 }
